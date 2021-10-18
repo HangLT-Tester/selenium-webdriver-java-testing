adeef690df21165c918d7225b023ffe49a3d5782
HangLT-Tester <83696897+HangLT-Tester@users.noreply.github.com>
2021-10-18 17:26:28 +0700

    Update Topic_05_Web_Element_Method.java

--- selenium/webdriver/Topic_05_Web_Element_Method.java
+++ selenium/webdriver/Topic_05_Web_Element_Method.java
@@ -1,11 +1,13 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -48,8 +50,27 @@ public class Topic_05_Web_Element_Method {
 		// Đêm xem có bao nhiêu element thỏa mãn đk
 		// Verify số lượng element tả về như mong đợi
 		// Thao tác với tất cả các loại element giống nhau trong 1 page (checkbox, textbox...)
+	List<WebElement> checkboxes = driver.findElements(By.xpath("//div[@class='inputs']/input[not(@type='checkbox')]"));
 		
+		//Lấy ra số lượng
+		checkboxes.size();
 		
+		//Verify có đúng 6 texbox tại form Đăng ký
+		Assert.assertEquals(checkboxes.size(), 6);
+		
+		WebElement singleElement = driver.findElement(By.className(""));
+		
+		//Texbox, TexArrea, Edittable dropdown
+		//Dữ liệu được toàn vẹn
+		singleElement.clear();
+		singleElement.sendKeys("");
+		
+		//Button, Link, Radio, Checkbox, Custum Dropdown,...
+		singleElement.click();
+		
+		//Các hàm có tiền tố bắt đầu băng Get luôn luôn trả về dữ liệu
+		// getTitle, getCurrentUrl, getPageSource, getAttribute, getCssValue, getText,...
+		singleElement.getAttribute("");
 	}
 
 
