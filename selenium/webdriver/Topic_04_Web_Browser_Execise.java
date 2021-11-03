c02e1a568c885946ebd91695a0b2b6e991211a02
HangLT-Tester <83696897+HangLT-Tester@users.noreply.github.com>
2021-11-03 16:32:57 +0700

    Topic_04_Web_Browser_Execise
    Bài tập

--- selenium/webdriver/Topic_04_Web_Browser_Execise.java
+++ selenium/webdriver/Topic_04_Web_Browser_Execise.java
@@ -2,9 +2,10 @@ package webdriver;
 
 import java.util.concurrent.TimeUnit;
 
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,26 +17,63 @@ public class Topic_04_Web_Browser_Execise {
 	public void beforeClass() {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("");
+		driver.get("http://live.techpanda.org/");
 	}
 
 	@Test
-	public void TC_01_() {
+	public void TC_01_Verify_URL() {
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/login/");
 		
+		driver.findElement(By.xpath("//div[@class='buttons-set']/a[@class='button']")).click();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/create/");
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_02_Verify_Title() {
+		driver.findElement(By.xpath("//*[@id='header']//a[@class='logo']")).click();
 		
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getTitle(), "Customer Login");
+		
+		driver.findElement(By.xpath("//div[@class='buttons-set']/a[@class='button']")).click();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
 	}
-
 	@Test
-	public void TC_03_() {
+	public void TC_03_Verify_Navigatetion() {
+		driver.findElement(By.xpath("//*[@id='header']//a[@class='logo']")).click();
 		
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("//div[@class='buttons-set']/a[@class='button']")).click();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/create/");
+		
+		driver.navigate().back();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/login/");
+		
+		driver.navigate().forward();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
 	}
 	@Test
 	public void TC_04() {
-		// ...
+		driver.findElement(By.xpath("//*[@id='header']//a[@class='logo']")).click();
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		sleepInSecond(3);
+		// Khai báo và khởi tạo biến tại vị trí màn hình Login
+		String currentPageSouce = driver.getPageSource();
+		Assert.assertTrue(currentPageSouce.contains("Login or Create an Account"));
+		
+		driver.findElement(By.xpath("//div[@class='buttons-set']/a[@class='button']")).click();
+		sleepInSecond(3);
+		// Khởi tạo lại giá trị mới tại màn hình Register
+		currentPageSouce = driver.getPageSource();
+		Assert.assertTrue(currentPageSouce.contains("Create an Account"));
 	}
 
 	@AfterClass
